1d4feb7d6ab3bfa3408160bd8f95a4327db505a9
Erik Lindén <erik.linden42@gmail.com>
2012-09-14 21:11:14 +0200

    Added MicroBan levels.
    Now there is a switch in BoardTester that allows
    using the m1.txt MicroBan levels. These levels are
    actually tracktable, unlike the all.scl levels.

--- src/BoardTester.java
+++ src/BoardTester.java
@@ -9,29 +9,58 @@ public class BoardTester {
 	 * @throws IOException 
 	 */
 	public static void main(String[] args) throws IOException {
-
-		FileInputStream fstream = new FileInputStream(new File("data", "all.slc"));
+		
+		final int file = 0;
+		String fileName = null;
+		
+		switch(file) {
+		case 0:
+			fileName = "m1.txt";
+			break;
+		case 1:
+			fileName = "all.slc";
+			break;
+		}
+		
+		FileInputStream fstream = new FileInputStream(new File("data", fileName));
 
 		DataInputStream in = new DataInputStream(fstream);
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		String strLine;
 
 		strLine = br.readLine();
-
-		while(0 != strLine.compareTo("; 108")) {
-			strLine = br.readLine();
-		}
-		
 		Vector<String> lines = new Vector<String>();
 		
-		strLine = br.readLine();
-		strLine = br.readLine();
-		while(strLine.length() != 0) {
-			lines.add(strLine);
-			System.out.println(strLine);
-			
+		switch(file) {
+		case 0:
+			while(0 != strLine.compareTo("; 10")) {
+				strLine = br.readLine();
+			}
+						
 			strLine = br.readLine();
+			strLine = br.readLine();
+			while(strLine.length() != 0) {
+				lines.add(strLine);
+				System.out.println(strLine);
+				
+				strLine = br.readLine();
+			}
+			break;
+		case 1:
+			while(0 != strLine.compareTo(";LEVEL 2080")) {
+				strLine = br.readLine();
+			}
+						
+			strLine = br.readLine();
+			while(strLine.charAt(0) != ';') {
+				lines.add(strLine);
+				System.out.println(strLine);
+				
+				strLine = br.readLine();
+			}
+			break;
 		}
+		
 		
 		String sol = Solver.solve(lines);
         
