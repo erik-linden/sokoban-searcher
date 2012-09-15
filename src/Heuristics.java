5e543ce8812772c7f793d0f81827f7b515f4b44d
Erik Lindén <erik.linden42@gmail.com>
2012-09-15 13:42:12 +0200

    Introduced a bi-partite matching heuristics.
    The heuristics now finds the real distance between all boxes and goals.
    Boxes are then matched to goals in such a way to minimize the total path.
    This is really slow, but never under-estimates the number of moves
    required.
    
    The matching is done by source code I pulled from the Internet.
    It is clerly marked with its origin, the code of honor says this is okay
    (I think) and it is not really an AI algorithm. Never the less, we should
    think about it.

--- src/Heuristics.java
+++ src/Heuristics.java
@@ -1,25 +1,80 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Vector;
+
 
 public class Heuristics {
-	
+
 	public static int calculateHeuristic(State state) {
-		int distance = 0;
-		
+		int[][] costs = new int[Board.nGoals][Board.nGoals];
+
+		int i = 0;
 		for(BoardPosition boxPos : state.boxPositions) {
-			int minDist = Integer.MAX_VALUE;
-			
-			for(BoardPosition goalPos : Board.goalPositions) {
-				int currDist = 0;
-				currDist += Math.abs(boxPos.row-goalPos.row);
-				currDist += Math.abs(boxPos.col-goalPos.col);
-				
-				if(currDist < minDist) {
-					minDist = currDist;
-				}
-			}
-			
-			distance += minDist;
+			costs[i] = listGoalDistances(state, boxPos);
+			i++;
 		}
 		
-		return distance;
+		int minCost = HungarianAlgorithm.hgAlgorithm(costs, "min");
+		return minCost;
+	}
+
+	private static int[] listGoalDistances(State state, BoardPosition start) {
+		byte[][] distMat = new byte[Board.rows+2][Board.cols+2];
+		byte goalsFound = 0;
+
+		Queue<BoardPosition> nodesToCheck = new LinkedList<BoardPosition>();
+		nodesToCheck.add(start);
+
+		while(!nodesToCheck.isEmpty() && goalsFound<Board.nGoals) {
+			BoardPosition current = nodesToCheck.poll();
+			byte childDist = (byte) (distMat[current.row][current.col]+1);
+
+			Vector<BoardPosition> children = current.makeAllChildren();
+			for(BoardPosition child : children) {
+				boolean notChecked = distMat[child.row][child.col]==0;
+				boolean notIsWall  = !Board.wallAt(child.row, child.col);
+				boolean notIsDead  = !Board.deadAt(child.row, child.col);
+
+				if(notChecked && notIsWall && notIsDead) {
+					distMat[child.row][child.col] = childDist;
+					nodesToCheck.add(child);
+					
+					if(Board.goalAt(child.row, child.col)) {
+						goalsFound++;
+					}
+				}
+			}
+
+		}
+		distMat[start.row][start.col] = 0;
+
+		int[] goalDist = new int[Board.goalPositions.size()];
+		byte i = 0;
+
+		for(BoardPosition goal : Board.goalPositions) {
+			goalDist[i] = distMat[goal.row][goal.col];
+			i++;
+		}
+
+		return goalDist;
+	}
+
+	static String distancesToString(byte[][] distMat) {
+		String result = "";
+
+		for(byte i=1; i<=Board.rows; i++) {
+			for(byte j=1; j<=Board.cols; j++) {
+				byte d = distMat[i][j];
+				if(d==0) {
+					result += "|  ";
+				}
+				else{
+					result += String.format("|%2d", d);
+				}
+			}
+			result += "|\n";
+		}
+
+		return result;
 	}
 }
