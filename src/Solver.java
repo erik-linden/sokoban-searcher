9e3fc3c8c7c62bab083c8bd847336b4618af18f2
Erik Lindén <erik.linden42@gmail.com>
2012-09-15 13:50:39 +0200

    Added an IDA* search algorithm.
    It is supposed to do a depth limited DFS, taking the remaining cost
    (as given by the heuristics) into account.
    
    Someone should probably check it for correctness

--- src/Solver.java
+++ src/Solver.java
@@ -1,18 +1,21 @@
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.PriorityQueue;
+import java.util.Stack;
 import java.util.Vector;
 
 
 public class Solver {
-	
+
 	public static String solve(Vector<String> lines) {
 		Board board = new Board(lines);
 		System.out.println(board.toString());
-		
-		State solvedState = aStar();
+
+		State solvedState = idaStar();
 		String revSoloution = solvedState.backtrackSolution();
 		new Guireplay(solvedState);
-		
+
 		return  new StringBuffer(revSoloution).reverse().toString();
 	}
 
@@ -20,33 +23,86 @@ public class Solver {
 		HashSet<State> visited = new HashSet<State>();
 		PriorityQueue<State> nodesLeft = new PriorityQueue<State>();
 		Vector<State> childStates = new Vector<State>();
-		
+
 		nodesLeft.add(Board.initialState);
-		
+
 		while(!nodesLeft.isEmpty()) {
 			State parent = nodesLeft.poll();
 			visited.add(parent);
-			
+
 			if(parent.isSolved()) {
 				System.out.println("Solved in "+parent.nPushes+" pushes.");
 				return parent;
 			}
-			
+
 			parent.getPushStates(childStates);
-			
+
 			for(State child : childStates) {
-				
+
 				if(visited.contains(child)) {
 					continue;
 				}
-				
+
 				if(!nodesLeft.contains(child)) {
 					nodesLeft.add(child);
 				}
-				
+
 			}
 		}
-		
+
 		return null;
+	}
+
+	private static State idaStar() {
+		HashSet<State> visited = new HashSet<State>();
+		Stack<State> nodesLeft = new Stack<State>();
+		Vector<State> childStates = new Vector<State>();
+
+		int cutoff = Board.initialState.heuristicValue;
+
+		while(true) {
+			int nextCutoff = Integer.MAX_VALUE;
+			nodesLeft.push(Board.initialState);
+			visited.clear();
+			
+			System.out.println("Search depth: "+cutoff);
+			while(!nodesLeft.isEmpty()) {
+				State parent = nodesLeft.pop();
+
+				if(!visited.contains(parent)) {
+					visited.add(parent);
+
+					parent.getPushStates(childStates);
+					Collections.sort(childStates);
+
+					for(State child : childStates) {
+						
+						if(visited.contains(child)) {
+							continue;
+						}
+
+						if(child.isSolved()) {
+							System.out.println("Solved in "+child.nPushes+" pushes.");
+							return child;
+						}
+
+						int childCost = child.nPushes+child.heuristicValue;
+						if(childCost > cutoff) {
+							nextCutoff = Math.min(nextCutoff, childCost);
+						}
+						else if(!nodesLeft.contains(child)) {
+							nodesLeft.add(child);
+						}
+
+					}
+				}
+			}
+			if(cutoff < nextCutoff) {
+				cutoff = nextCutoff;
+			}
+			else {
+				return null;
+			}
+		}
 	}
 }
